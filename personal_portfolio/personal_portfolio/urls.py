2b4a02824ab53d0ce5762971427feb980fb4c948
Viera Pixová <pixovaviera@gmail.com>
2021-01-22 21:19:36 +0100

    Fixed favicon error.

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
+    path('favicon.ico/', RedirectView.as_view(url='/static/image/favicon.ico')),
 ]
